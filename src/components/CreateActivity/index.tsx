ae38fc0437903ccde9f8f0246cd18b7772f12202
Bruna Schneiders <bruna.schneiders@ifood.com.br>
2024-06-21 21:12:01 -0300

    refactor: Reorganize activity creation

--- src/components/CreateActivity/index.tsx
+++ src/components/CreateActivity/index.tsx
@@ -1,52 +1,13 @@
 import { useState } from "react";
-import { Box, Button, Dialog, TextField, Typography } from "@mui/material";
+import { Button } from "@mui/material";
 import { Add } from "@mui/icons-material";
-import { DateTimePicker } from "@mui/x-date-pickers";
-import { useForm, Controller, SubmitHandler } from "react-hook-form";
-import useCreateActivity from "../../hooks/useCreateActivity";
-import { Activity } from "../../types";
-import dayjs from "dayjs";
-import { Snackbar } from "../Snackbar";
-import { useCalendar } from "../../providers/CalendarProvider/useCalendar";
-
-type FormValues = Activity;
+import { ActivityForm } from "../ActivityForm";
 
 export const CreateActivity = () => {
-  const { mutate: createActivity, isLoading } = useCreateActivity();
-  const { selectedDate } = useCalendar();
-  const {
-    control,
-    handleSubmit,
-    formState: { isValid },
-    reset,
-  } = useForm<FormValues>({
-    defaultValues: {
-      name: "",
-      description: "",
-      date: selectedDate ? selectedDate : undefined,
-    },
-  });
-
   const [isOpen, setIsOpen] = useState(false);
-  const [isSuccessSnackbarOpen, setIsSuccessSnackbarOpen] = useState(false);
-  const [isErrorSnackbarOpen, setIsErrorSnackbarOpen] = useState(false);
 
   const handleClose = () => setIsOpen(false);
   const handleOpen = () => setIsOpen(true);
-
-  const onSubmit: SubmitHandler<FormValues> = (data) => {
-    createActivity(data, {
-      onSuccess: () => {
-        reset();
-        handleClose();
-        setIsSuccessSnackbarOpen(true);
-      },
-      onError: (error) => {
-        console.error(error);
-        setIsErrorSnackbarOpen(true);
-      },
-    });
-  };
 
   return (
     <>
@@ -60,91 +21,7 @@ export const CreateActivity = () => {
         Cadastrar atividade
       </Button>
 
-      <Dialog open={isOpen} onClose={handleClose}>
-        <Box
-          component="form"
-          onSubmit={handleSubmit(onSubmit)}
-          display="flex"
-          flexDirection="column"
-          gap="16px"
-          padding="64px"
-          minWidth={600}
-        >
-          <Typography variant="h3" fontSize="24px" fontWeight="bold" mb="16px">
-            Cadastrar Atividade
-          </Typography>
-
-          <Controller
-            name="name"
-            control={control}
-            rules={{ required: "Campo obrigatório" }}
-            render={({ field }) => (
-              <TextField {...field} label="Nome" variant="outlined" />
-            )}
-          />
-          <Controller
-            name="description"
-            control={control}
-            rules={{ required: "Campo obrigatório" }}
-            render={({ field }) => (
-              <TextField
-                {...field}
-                label="Descrição"
-                variant="outlined"
-                multiline
-                rows={4}
-              />
-            )}
-          />
-          <Controller
-            name="date"
-            control={control}
-            rules={{ required: "Campo obrigatório" }}
-            render={({ field }) => (
-              <DateTimePicker
-                {...field}
-                ampm={false}
-                value={field.value ? dayjs(field.value) : null}
-                onChange={(value) => field.onChange(value?.toDate())}
-                minDate={dayjs().startOf("hour")}
-              />
-            )}
-          />
-
-          <Box display="flex" justifyContent="flex-end" gap="12px" mt="24px">
-            <Button
-              variant="outlined"
-              size="large"
-              onClick={handleClose}
-              disabled={isLoading}
-            >
-              Cancelar
-            </Button>
-            <Button
-              type="submit"
-              variant="contained"
-              size="large"
-              disabled={!isValid || isLoading}
-            >
-              Salvar
-            </Button>
-          </Box>
-        </Box>
-      </Dialog>
-
-      <Snackbar
-        isOpen={isSuccessSnackbarOpen}
-        onClose={() => setIsSuccessSnackbarOpen(false)}
-        variant="success"
-        message="Atividade cadastrada com sucesso"
-      />
-
-      <Snackbar
-        isOpen={isErrorSnackbarOpen}
-        onClose={() => setIsErrorSnackbarOpen(false)}
-        variant="error"
-        message="Erro ao cadastrar atividade"
-      />
+      {isOpen && <ActivityForm isOpen={isOpen} handleClose={handleClose} />}
     </>
   );
 };
--- src/hooks/useCreateActivity.ts
+++ src/hooks/useCreateActivity.ts
@@ -1,16 +1,23 @@
-import { useMutation } from "react-query";
+import { useMutation, useQueryClient } from "react-query";
 import axios from "axios";
 import { Activity } from "../types";
 import { BASE_URL, ELDERLY_ID, NURSE_ID } from "../constants";
 
 const useCreateActivity = () => {
-  return useMutation((activityData: Activity) => {
-    return axios.post(`${BASE_URL}/activity`, {
-      ...activityData,
-      elderlyId: ELDERLY_ID,
-      nurseId: NURSE_ID,
-    });
-  });
+  const queryClient = useQueryClient();
+
+  return useMutation(
+    (activityData: Activity) => {
+      return axios.post(`${BASE_URL}/activity`, {
+        ...activityData,
+        elderlyId: ELDERLY_ID,
+        nurseId: NURSE_ID,
+      });
+    },
+    {
+      onSuccess: () => queryClient.invalidateQueries("activities"),
+    }
+  );
 };
 
 export default useCreateActivity;
